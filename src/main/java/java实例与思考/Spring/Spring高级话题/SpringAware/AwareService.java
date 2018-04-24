908b0043a0e1750964d15ed3223f2ff4ac2e8c0e
Administrator <2451222478@qq.com>
2018-04-24 22:17:17 +0800

    导入爱机，彻底用maven

---
+++ src/main/java/java实例与思考/Spring/Spring高级话题/SpringAware/AwareService.java
@@ -0,0 +1,36 @@
+package java实例与思考.Spring.Spring高级话题.SpringAware;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class AwareService implements BeanNameAware, ResourceLoaderAware {
+    private String beanName;
+    private ResourceLoader loader;
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.loader = resourceLoader;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+    }
+
+    public void outputResult() {
+        System.out.println("Bean的名称为:" + beanName);
+        Resource resource = loader.getResource("classpath:java实例与思考/Spring/Spring高级话题/SpringAware/test.txt");
+        try {
+            System.out.println("ResourceLoader加载的文件内容为:" + IOUtils.toString(resource.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
