86fc92cc8bae1c111803f56fb64a864a14d02f73
Administrator <2451222478@qq.com>
2018-04-27 23:01:48 +0800

    Spring多线程

---
+++ src/main/java/java实例与思考/Spring/Spring高级话题/多线程/TaskExecutorConfig.java
@@ -0,0 +1,32 @@
+package java实例与思考.Spring.Spring高级话题.多线程;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@ComponentScan("java实例与思考.Spring.Spring高级话题.多线程")
+@EnableAsync   //开启异步任务支持
+public class TaskExecutorConfig implements AsyncConfigurer {//配置类实现AsyncConfigurer接口，重写
+    // getAsyncExecutor方法，并返回ThreadPoolTaskExecutor，获得基于线程池的TaskExecutor
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(5);
+        taskExecutor.setMaxPoolSize(10);
+        taskExecutor.setQueueCapacity(25);
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return null;
+    }
+}
